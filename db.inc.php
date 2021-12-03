a082096e8fc9df9856586c9031fa73c3fa9745e1
Jonathan Guerra <jonathanhttp123@gmail.com>
2021-12-03 00:09:07 -0500

    json

---
+++ db.inc.php
@@ -0,0 +1,5 @@
+<?php
+$servername = "localhost";
+$username = "jonathan";
+$password = "Admin*1234";
+$dbname = "myDB";
---
+++ deber_json.php
@@ -0,0 +1,24 @@
+<?php
+include "db.inc.php";
+
+$conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password );
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$que = "SELECT * FROM myguests";
+$resultado = $conn->prepare($que);
+$resultado->execute();
+
+//variable tipo array
+$json = array();
+
+while ($row = $resultado->fetch(PDO::FETCH_ASSOC)){
+    $json ['USUARIOS'][] = $row;
+}
+echo json_encode($json);
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <br><a href="index.html">Registrar nuevo usuario</a>
+    </body>
+</html>
+
